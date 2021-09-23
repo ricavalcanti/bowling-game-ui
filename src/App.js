c721fd7fc7a364b2a17d7251eeae5bde84bd7bf3
Ricardo <ricavalcanti@bct.ect.ufrn.br>
2021-09-22 21:34:50 -0300

    feat: addding react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import InitialPage from './view/pages/initial-page';
 import theme from './theme';
 
 const App = () => (
-  <div className="App">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <InitialPage />
-    </ThemeProvider>
-  </div>
+  <Router>
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Switch>
+          <Route exact path="/" component={InitialPage} />
+          <Route default component={InitialPage} />
+        </Switch>
+      </ThemeProvider>
+    </div>
+  </Router>
 );
 
 export default App;
